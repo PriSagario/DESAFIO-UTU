5382273a6d9d81223df0d257387d68b080fafcf2
Pri Sagario <priscilla.sagario@wuala.net>
2022-08-30 17:20:17 -0300

    finish

--- src/components/landing/TeamSection.js
+++ src/components/landing/TeamSection.js
@@ -4,7 +4,7 @@ import CarouselComponent from './Carousel';
 export default function TeamSection() {
     return (
         <section className="pt-4">
-            <div className="container max-w-7xl mx-auto px-4" id="Altimetrias" >
+            <div className="max-w-7xl mx-auto px-4" id="Altimetrias" >
                 <Title heading="ALTIMETRIAS" >
                     <CarouselComponent/>
                 </Title>
